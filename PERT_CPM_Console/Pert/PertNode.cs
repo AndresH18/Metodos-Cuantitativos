a10778e3d89c86088c86b9bf45e3bf885ee7e93d
andre <andres.hoyos18@eia.edu.co>
2022-09-29 17:18:37 -0500

    Finished Pert Program

---
+++ PERT_CPM_Console/Pert/PertNode.cs
@@ -0,0 +1,24 @@
+﻿namespace PERT_CPM_Console.Pert;
+
+public class PertNode : Node
+{
+    public double InitialValue { get; set; }
+    public double LikelyValue { get; set; }
+    public double LastValue { get; set; }
+
+    public override double Length => (InitialValue + 4 * LikelyValue + LastValue) / 6;
+
+    public double Deviation => (LastValue - InitialValue) / 6;
+
+    public PertNode()
+    {
+    }
+
+    public PertNode(string name, double initialValue, double likelyValue, double lastValue)
+    {
+        Name = name;
+        InitialValue = initialValue;
+        LikelyValue = likelyValue;
+        LastValue = lastValue;
+    }
+}
